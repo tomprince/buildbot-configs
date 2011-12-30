769e428a8dc6c8192b3ba0369465b2a084d7dfd0
Tom Prince <tom.prince@ualberta.net>
2011-12-30 16:44:29 -0500

    Update win32 libraries.

--- win32-dist-install.cmake
+++ win32-dist-install.cmake
@@ -1,8 +1,12 @@
+FILE(GLOB SDL_DLLS
+	"C:/Program Files/SDL_ttf-2.0.10/lib/*.dll"
+	"C:/Program Files/SDL_mixer-1.2.11/lib/*.dll"
+	)
 INSTALL( FILES
 	"C:/Program Files/SDL-1.2.14/lib/SDL.dll"
+	${SDL_DLLS}
 	"C:/windows/system32/python26.dll"
 	"C:/Program Files/openal-soft-1.12.854-bin/win32/OpenAL32.dll"
-	"C:/Program Files/Cmake 2.8/bin/MSVCR90.dll"
 	DESTINATION ${CMAKE_INSTALL_PREFIX})
 if (MINGW)
 	INSTALL( FILES
